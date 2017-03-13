7aaa60e8f7f093e3570a4cfd05d1238d1f95dbeb
unknown <deasley@1005-7.eecs.ku.edu>
2017-03-13 14:55:45 -0500

    Created matrixCalc.java
    Added main method and a check of the argument length.

---
+++ matrixCalc.java
@@ -0,0 +1,15 @@
+
+public class matrixCalc {
+
+	public static int main(String[] args) {
+		//Confirm command-line parameters
+		if(args.length < 3)
+		{
+			System.out.println("Error - Expected usage: ./main input.txt output.txt");
+			return 1;
+		}
+		
+		return 0;
+	}
+
+}
