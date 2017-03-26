4a2b1ba2930dfea89afa1358c1cbea976b081846
David Easley <David Easley>
2017-03-26 15:02:49 -0500

    Added determinator function to Matrix.java
    The function is presently non-functional as the subMatrix referenced
    within has yet to be implemented.

--- Matrix.java
+++ Matrix.java
@@ -16,5 +16,21 @@ public class Matrix {
 		innards[row] = values;
 	}
 	
-	
+	public double determinator(){
+		double arnold = 0.0;
+		
+		if (1 == size){
+			arnold = innards[0][0];
+		}
+		else if(2 == size){
+			arnold = innards[0][0] * innards[1][1] - innards[0][1] * innards[1][0];
+		}
+		else
+		{
+			for(int i = 0; i < size; i++){
+				arnold += Math.pow(-1.0, (double)i) * innards[0][i] * subMatrix(0, 1).determinent();
+			}
+		}
+		return arnold;
+	}
 }
