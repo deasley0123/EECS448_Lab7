898afcabf91b3f34cc70f4771cf77fb9d38dce6c
David Easley <David Easley>
2017-03-26 14:44:13 -0500

    Added setRow function to Matrix class
    Rows in a matrix' inards can now be set externally. The row number and an
    array of values is passed.

--- Matrix.java
+++ Matrix.java
@@ -7,4 +7,11 @@ public class Matrix {
 		size = n;
 		inards = new int[size][size];
 	}
+	
+	public void setRow(int row, int[] values){
+		for(int i = 0; i < values.length; i++){
+			inards[row][i] = values[i];
+		}
+	}
+	
 }
