55b017f120c2c7d09c83043ee29cdcae30714a18
David Easley <David Easley>
2017-03-26 14:11:48 -0500

    Created and initialized read/write streams
    Initialization was performed within a try-catch block to escape if the
    user provided in/out file names are not good. Additionally, I started
    mapping out the core process loop.

--- matrixCalc.java
+++ matrixCalc.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class matrixCalc {
 
@@ -6,6 +11,34 @@ public class matrixCalc {
 		if(args.length < 3)
 		{
 			System.out.println("Error - Expected usage: ./main input.txt output.txt");
+			return 1;
+		}
+		
+		// Initialize file streams
+		BufferedReader buffIn;
+		BufferedWriter buffOut;
+		
+		try {
+			buffIn = new BufferedReader(new FileReader(args[1]));
+			buffOut = new BufferedWriter(new FileWriter(args[2]));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return 1;
+		}
+
+		//Process commands
+		int n;
+		try {
+			
+			while((n = Integer.parseInt(buffIn.readLine())) != 0){
+				
+			}
+			
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+			return 1;
+		} catch (IOException e) {
+			e.printStackTrace();
 			return 1;
 		}
 		
